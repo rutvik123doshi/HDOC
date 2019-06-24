210dc720981aa9c7912d8bdd81b19a9f0da51bd9
rutvik123doshi <rutvik123doshi@gmail.com>
2019-06-24 23:28:37 +0530

    Update HackerEarth_WinTheGame.java
    Improved Code without unnecessary comments

--- HackerEarth_WinTheGame.java
+++ HackerEarth_WinTheGame.java
@@ -14,37 +14,24 @@ import java.util.*;
 class TestClass {
     
     public static void solve(int r, int b){
-        // dp wala solution
-        // float prods[] = new float[r];
+        
+        # Remove Trivial Condtions
         if (r==0 ){
             System.out.println("1.000000");
             return;
         }
-        float sum = ((float)r/ (r+b));
-        float previous = 1;
-        float current ;
-        // r--;
-        sum=0;
-        // if (sum < 1 && sum>0){
 
-        //   for(int i =0; i<r; i++){
-           while(true){
-            // if(b==0){
-            //     // do something
-            //     break;
-            // }
-            
-            // red ko jitao
+        float previous = 1, current, sum=0;
+        while(true){
+            // Make sure red wins
             if(previous==0){
                 break;
             }
             
             current = previous * ((float) (r)/ (r+b));
             sum += current;
-            // check for exit cases;
-
             
-
+            // check for exit cases;
             if(b<2){
                 break;
             }
@@ -52,31 +39,13 @@ class TestClass {
             b--;
             previous *=  ((float) b/ (r+b)) ;
             b--;
-            
-            
-            
-            // current = previous* ((float) b/ (r+b));
-            // if(r==0){
-            //     break;
-            // }
-            // b--;
-            // current *=  ((float) (r)/ (r+b));
-            // r--;
-            // sum += current ;
-        
-            // break when prods[i] is 0
-            
-            } 
-        // }
+
+        } 
         System.out.printf("%.6f\n",sum);
-        
-        
+                
     }
     
     public static void main(String args[] ) throws Exception {
-        /* Sample code to perform I/O:
-         * Use either of these methods for input */
-
         //BufferedReader
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int t = Integer.parseInt(br.readLine()) ;
@@ -87,7 +56,5 @@ class TestClass {
             int b = Integer.parseInt(st.nextToken()) ;
             solve(a, b);
         }
-        // Write your code here
-
     }
 }
